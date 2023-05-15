286c97e145672a2f4f2949dfa5ead5d1af150986
mass.web.app <mass.web.app@gmail.com>
2023-05-15 13:31:54 -0700

    Initial project

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->createAdminUser();
+        $this->createUser();
+    }
+
+    private function createAdminUser()
+    {
+        User::query()->create([
+            'type'=>User::TYPE_ADMIN,
+            'name'=>'مدیر اصلی',
+            'email'=>'admin@gmail.com',
+            'mobile'=>'+989358891586'
+        ]);
+        $this->command->info('CREATE ADMIN USER');
+    }
+
+    private function createUser()
+    {
+        User::query()->create([
+            'type'=>User::TYPE_USER,
+            'name'=>'کاربر اول',
+            'email'=>'user1@gmail.com',
+            'mobile'=>'+989358891581'
+        ]);
+        $this->command->info('CREATE User 1');
+    }
+}
